8f68e05fcac14b27f32833db6ab3a4d6734d43d1
FlorianPoessl <florian.poessl97@gmail.com>
2020-01-27 23:40:08 +0100

    small bugfix batch file

--- start_semantic_web.bat
+++ start_semantic_web.bat
@@ -1,8 +1,8 @@
-cd %~dp0\dke_webservice_turtle
+cd %~dp0\webservice_turtle
 start start_webservice_turtle.bat
-cd %~dp0\dke_webservice_rdfxml
+cd %~dp0\webservice_rdfxml
 start start_webservice_rdfxml.bat
-cd %~dp0\dke_metaservice
+cd %~dp0\metaservice
 start start_metaservice.bat
 cd %~dp0\fuseki_metaservice
 start start_fuseki_metaservice.bat
